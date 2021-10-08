30cf4ed2ef0d4568cc26198a71de9f90fac84cc1
Jason Pires <jason.pires@outlook.com>
2021-10-08 02:27:34 -0300

    Commit inicial

---
+++ src/test/java/com/jason/microservice/demoMS/DemoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.microservice.demoMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
